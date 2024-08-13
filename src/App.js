7402a1569d9248b838cbc2b96cf7955ac20bdfc8
FazSyed <fazsyed1@gmail.com>
2024-08-13 23:24:40 +0400

    Created Routes for page switching

--- src/App.js
+++ src/App.js
@@ -3,17 +3,40 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import RecommendedVideos from "./components/RecommendedVideos/RecommendedVideos";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchPage from "./components/Search/SearchPage";
 
 const App = () => {
   return (
-    // Class Naming convention - BEM
     <div className="app">
-      <Header />
-
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
+      <Router>
+        {/* The Header will always appear, regardless of which page we are in... */}
+        <Header />
+        <Routes>
+          <Route
+            path="/search/:searchTerm"
+            element={
+              <>
+                <div className="app__page">
+                  <Sidebar />
+                  <SearchPage />
+                </div>
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="app__page">
+                  <Sidebar />
+                  <RecommendedVideos />
+                </div>
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 };
