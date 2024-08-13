dd614706c4f65c3b27ee0a6f7421b43a76bb15e0
FazSyed <fazsyed1@gmail.com>
2024-08-13 12:50:10 +0400

    Created another conditional render for the You row

--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -5,13 +5,22 @@ import { Avatar } from "@mui/material";
 const SidebarRow = ({ selected, title, Icon, src }) => {
   return (
     <div className={`sidebarRow ${selected && "selected"}`}>
-      {src ? (
-        <Avatar src={src} className="sidebarRow__avatar" />
+      {title === "You" ? (
+        <>
+          <p className="sidebarRow__titleYou">{title}</p>
+          <Icon className="sidebarRow__iconYou" />
+        </>
       ) : (
-        <Icon className="sidebarRow__icon" />
-      )}
+        <>
+          {src ? (
+            <Avatar src={src} className="sidebarRow__avatar" />
+          ) : (
+            <Icon className="sidebarRow__icon" />
+          )}
 
-      <p className="sidebarRow__title">{title}</p>
+          <p className="sidebarRow__title">{title}</p>
+        </>
+      )}
     </div>
   );
 };
