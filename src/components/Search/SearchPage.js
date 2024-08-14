db491cec1aa06acc13d7617c1cc891b98d030c7d
FazSyed <fazsyed1@gmail.com>
2024-08-14 16:31:47 +0400

    Added more Video Rows

--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -81,7 +81,7 @@ const SearchPage = () => {
         logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
         channel="Clever Programmer"
         verified
-        description="We just created the Best Free JavaScript Programming Course on the planet Made with l..."
+        description="We just created the Best Free JavaScript Programming Course on the planet Made with lots of ❤️. Kickstart your Web ..."
       />
       <VideoRow
         image="https://i.ytimg.com/vi/pnkuI8KXW_8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDVFScKEytwp4G7d1NAvTZ7ZkZjLg"
@@ -92,6 +92,66 @@ const SearchPage = () => {
         channel="Clever Programmer"
         verified
         description="In this FREE LIVE training, Qazi & Sonny will show you how to build a Spotify Clone with REACT JS . #reactjs #spotify ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/FNGoExJlLQY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA8FfDs2vQVtDdEZPI0LASbFb8r_A"
+        title="HTML Crash Course for Absolute Beginners 2020 [Tutorial]"
+        views="215K"
+        timestamp="4 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="TABLE OF CONTENTS... ENJOY 5:30 - HTML Header Tags 6:00 - HTML File 10:30 - Paragraph Tags 13:35 ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/f7T48W0cwXM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBCKkoLkdh-GjL_E5jcWBGUPBG8QA"
+        title="🔴 How to Build a Instagram Clone with REACT JS for Beginners (in 3 Hours!)"
+        views="546K"
+        timestamp="Streamed 4 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="In this FREE LIVE training, Qazi and Sonny will show you how simple it is to create a full stack, INSTAGRA ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/Ym6RYyRBPaA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAFQD4eoOszUgiKWJxdTo95Yyh-Xw"
+        title="🔴 Build Modern Portfolio with NextJS, TypeScript, SSR, and React Bricks CMS"
+        views="168K"
+        timestamp="2 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="Here's a modern portfolio you could have to impress an employer Build it with me & add your own ✨sa ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/0mVbNp1ol_w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2EpGWB4EsWmk5fhbtYw-BZ28tvQ"
+        title="ReactJS Tutorial for Beginners - Full Course in 20 Hours [2022]"
+        views="529K"
+        timestamp="2 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="In this tutorial you're going to learn all about: Functional components in React JS Props and States in R ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/-cMqr9HpZ-Y/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDQLOMEQB703FbgaDArfjEGamxbjQ"
+        title="React JS Tutorial for Beginners - Full Course in 12 Hours [2021]"
+        views="497K"
+        timestamp="3 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="Here is the best free React JS programming course on the planet. Made with lots of ❤️. Take your web ..."
+      />
+      <VideoRow
+        image="https://i.ytimg.com/vi/KN6oBEOz2ZI/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNcVB6a02Xy2aafRBoUkHqDZyShA"
+        title="CSS Crash Course for Absolute Beginners - Full Course"
+        views="213K"
+        timestamp="4 years"
+        logo="https://yt3.googleusercontent.com/ytc/AIdro_kotL-OQVXsay2vKRujBvNWcY47UZUwC-axNozc8Mzdutk=s160-c-k-c0x00ffffff-no-rj"
+        channel="Clever Programmer"
+        verified
+        description="TABLE OF CONTENTS... ENJOY 2:09 - What is CSS? 13:07 - The Best Framework for CSS / Front-End 47:1 ..."
       />
     </div>
   );
